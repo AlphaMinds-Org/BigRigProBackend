a499bae9b333d8ef3922f94d39361084d30406f3
Maduka Dilshan <42783431+MadukaDila@users.noreply.github.com>
2023-05-17 17:24:42 +0530

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -7,7 +7,7 @@ let conn = mysql.createConnection({
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,
-  connectionLimit: 10
+  connectionLimit: 1000
 })
 
 
