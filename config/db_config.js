d1cc383c489100416e17d98a8c7e56ad11518050
Maduka Dilshan <42783431+MadukaDila@users.noreply.github.com>
2023-05-17 17:40:28 +0530

    Update config

--- config/db_config.js
+++ config/db_config.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 var mysql = require('mysql2');
 
-let conn = mysql.createConnection({
+const conn = mysql.createConnection({
   host: process.env.DB_HOST,  
   port: process.env.DB_PORT,   
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,
-  connectionLimit: 1000
+  connectionLimit: 10
 })
 
 
